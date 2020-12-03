9db002abf527d6c6cf8f52edbcf7fe998880b286
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2020-12-03 19:16:08 +0100

    Translate to Swedish

--- script.js
+++ script.js
@@ -1,57 +1,94 @@
-// This file is where the magic happens! Step 1-6 contains an
-// explanation of the starter code. If you wan't to skip ahead
-// to the excersise - go to step 7. And as always, feel free to
-// change the styling and add more fields to the cards if you
-// find something interesting in the data.
+/* Det är i denna fil du ska jobba! Följ steg 1-7 för att få en
+förklaring på den kod som redan skrivits. Läs de i sifferordning
+för att det ska bli tydligt vad som sker. Om du vill hoppa direkt
+till övningen - gå till steg 8. Och som alltid - ändra gärna
+stylingen och lägg till fler fält i korten om du hittar något
+intressant i datan! */
 
-// 1. Here we declare the URL to the API.
+/* 1. Här deklarerar vi URL:en till API:et så vi vet vart vi ska
+skicka förfrågan.*/
 const URL = "https://api.spacexdata.com/v4/launches"
 
-// 5. This is our main function. It picks up the json we
-//    passed along when we invoked the function. So "launches"
-//    contains the whole response from the API since it's
-//    the same as "json".
+/* 2. Vi deklarer också dagens datum för att kunna
+avgöra om uppskjutningen har varit eller inte.*/
+const todaysDate = Math.round(new Date().getTime()/1000)
+
+/* 6. Det här är vår huvudfunktion. Den plockar upp datan (json)
+som vi skickade med när vi kallade på funktionen. Så 'launches'
+innehåller hela responsen från API:et eftersom det är detsamma som
+json (som vi skickade med). */
 const displayLaunches = launches => {
-// 6. We use the launches array and for each item (or launch)
-//    in the array, we add a new div with info inside:
+  /* 7. Om du kikat i konsollen har du nog sett att datan vi får
+  tillbaka är en array. För varje uppskjutning i arrayen skapar vi
+  en ny div med data i sig.*/
   launches.forEach(launch => {
-            
-// 7. We get the element from the HTML and change its inner HTML.
-//    We've already added the name of the launch, but your task is
-//    to look through the JSON and see if you can find more things
-//    to display about each launch, e.g. an image of the patch used,
-//    launch details and a link to read more about the launch.
-//    Hint! Look in the console where we logged the json to see how
-//    it's structured. Think of it as a regular JavaScript array
-//    with objects in it
-const details = launch.details
+         
+    /* 8. Vi hämtar elementet från HTML:en och ändrar dess HTML
+    med hjälp av innerHTML. Vi har redan lagt till namn och datum
+    för uppskjutningen - din uppgift är att lära känna datan och
+    lägga till fler saker att visa i varje kort, t.ex. en bild av
+    tygmärket (patch) för besätttningen, uppskjutningsdetaljer eller
+    en länk för att läsa mer om uppskjutningen.  */
+
+    /*Hint! Kika i konsollen där vi loggade datan för att se hur den
+    är strukturerad. Tänk på det som en vanlig JavaScript-array med
+    objekt i. */
+
+    /*Hint 2! Du kan välja om du antingen vill skriva in datan direkt
+    inuti <h2>${launch.name}</h2>, men om du t.ex. vill redigera värdet
+    kan det kännas snyggare att först lägga det i en variabel som vi
+    gjort nedan.*/
+
+    const name = launch.name
+    const date = launch.date_utc.slice(0,10)
+
+    /*Hint 3! Här är en funktion med villkor i som visar olika saker 
+    beroende på datum och värdet på launch.success. På ett liknande
+    sätt skulle du t.ex. göra en funktion som visar olika saker
+    beroende på om datan finns eller inte.*/
+    const checkSuccess = () => {
+      //Om dagens datum är mindre (tidigare) än launch date - returnera
+      //"Not yet launched"
+      if (todaysDate < launch.date_unix) {
+        return "Not yet launched"
+      }
+      //Om launch.success är true - returnera "Launch successful"
+      if (launch.success) {
+        return "Launch successful"
+      //Annars returnera "Launch failed"
+      } else {
+        return "Launch failed"
+      }
+    }
+
     document.querySelector(".all-launches").innerHTML += 
       `<div class="launch">
-        <h2>${launch.name}</h2>
-        <p>${launch.date_utc.slice(0,10)}</p>
+        <h2>${name}</h2>
+        <p>${checkSuccess()}
+        <p>${date}</p>
         <img src="" />
         <p class="details">Launch details</p>
         <a href="">Read more</a>
       </div>
       `
   })
-} 
 
-// 2. First thing that happens in this file is the fetch().
-//    Fetch is a built in JavaScript function that sends a
-//    request to the URL we pass as an argument. We turn the
-//    response to JSON format (JavaScript Object Notation)
-//    before we finally decide what to do with the data.
+} 
+/* 3.  Det första som sker i vår fil är denna fetch-funktionen.
+Fetch är en inbyggd JavaScript-funktion som skickar en förfrågan
+till den URL vi skriver inom parentesen. När vi fått svar omvandlar
+vi datan till JSON-format (JavaScript Object Notation) och därefter
+specificerar vi vad vi vill göra med datan. */
 fetch(URL)
   .then((response) => {
     return response.json()
   })
   .then((json) => {
-    // 3. Here we log the data to the console, to be able to see
-    //    how it's structured.
+    /* 4. Här loggar vi datan till konsollen för att se hur den är
+    strukturerad. Öppna konsollen i browsern för att kika! */
     console.log(json)
 
-    // 4. Here we call our displayLaunches function and pass along
-    //    the json/data as an argument.
+    /* 5. Här kallar vi på vår displayLaunches-funktion och skickar
+    med data/json som argument. */
     displayLaunches(json)
   })
